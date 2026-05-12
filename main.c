123a4d7f13d7281024b9c999cfa45e99aefddef0
Marc <marc@marcrobison.com>
2026-05-12 14:02:01 +0200

    feat: first working inline assembly snippet:
    write value 0 to working register w0 followed by writting that value
    into TRISA to set the direction to output.

--- main.c
+++ main.c
@@ -8,17 +8,20 @@
 
 #include "xc.h"
 
-#define LED_LATCH _LATA0
-#define LED_TRIS _TRISA0
 
 int main(void) {
     // Microstick II red LED is on PORT A, PIN 0
-    TRISA = 0;
+    //TRISA = 0;
+//    LATA = 1;
+    asm (
+    "MOV 0x0, W0\n"
+    "MOV W0, TRISA"
+    );
+    
     while(1) {
-        long long int i = 100000;
+        long int i = 0xffff; 
         while(i--);
-        LATA = !LATA ;
-
+        LATA = !LATA;
     }
     return 0;
 }
