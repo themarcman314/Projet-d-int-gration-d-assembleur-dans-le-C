21c56e6657ddcd4def634ea2a5d469e97c03f64e
Marc <marc@marcrobison.com>
2026-05-15 11:31:57 +0200

    feat: initial UART config

--- main.c
+++ main.c
@@ -7,26 +7,48 @@
 
 
 #include "xc.h"
+#include <string.h>
+
+void uart_init(void);
 
 
 int main(void) {
-    // Microstick II red LED is on PORT A, PIN 0
-    asm (
-    "MOV 0x0, WREG\n"
-    "MOV WREG, TRISA\n"
-    "MOV #0, W2"
-    );
+
+    uart_init();
     
-    asm (
-    "LOOP:\n"
-    "MOV #0xffff, W1\n" // reset counter variable
-    "LOOP_WAIT:\n"      // while counter is not over...
-    "SUB #1, W1\n"        // substract 1 from reset counter
-    "BRA NZ, LOOP_WAIT\n" // branch if result of previous operation is non 0
-    "COM LATA, WREG\n"  // store complement of LATA in WREG
-    "MOV WREG, LATA\n"  // toggle the LED
-    "BRA LOOP"          // do it all again
-    );
+    char msg[] = "Hello World!\r\n";
     
-    return 0;
+    while(1) {
+        for(int i=0; i < strlen(msg); i++) {
+            U1TXREG |= msg[i]; // write to tx buffer
+            //while()
+        }
+        unsigned long i = 0xffffff;
+        while(i--); // wait a little before sending again
+        
+    }
+}
+
+void uart_init(void) {
+    // we want to use pins 21 (RB10) and 22 (RB11)
+    // for UART
+    // RB10 as Rx and RB11 as Tx
+    // RP10 and RP11 are corresponding peripheral mux pin names
+    
+    // apparently once UART is enabled TRIS settings are overwritten anyways
+    // so this should be redundant
+    TRISB &= ~(1 << 10); // set RB11 as output by clearing the pin
+    TRISB |= 1 << 15; // set RB10 (pin21) to input
+    
+    // bits 0 to 4 of RPINR18 register set where the input pin of UART1 receive is mapped
+    // see DS70291G page 128
+    RPINR18 |= 10; // set RP10 to UART1 input
+    RPOR5 |= (0b00011 << 8); // set RP11 to UART1 output
+    
+    U1MODE |= 1 << 11; // disable flow control
+    U1MODE |= 1 << 15; // set UARTEN (UART enable bit)
+    U1STA |= 1 << 12; // Rx controlled by UART1
+    U1STA |= 1 << 10; // enable Tx
+    
+    //TODO: configure baudrate
 }
