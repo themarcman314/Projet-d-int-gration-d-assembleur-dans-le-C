129a5fab6905ef1808b8e1dd3460fd69dbae3fee
Marc Robison <marc@marcrobison.com>
2026-05-28 21:32:11 +0200

    feat: 2 loop filter

--- constants.h
+++ constants.h
@@ -8,13 +8,22 @@
 #ifndef CONSTANTS_H
 #define	CONSTANTS_H
 
+#include "dsp.h"
 #include <stdint.h>
 
 #define Q_SHIFT 15
 #define SCALE (1UL << Q_SHIFT)
 
+#define ALPHA 0.01
+#define STEP 0.9
+
 const uint16_t q_num_1 = (uint16_t)(0.5 * SCALE);
 const uint16_t q_num_2 = (uint16_t)(0.5 * SCALE);
+
+const uint16_t step_q = (uint16_t) (STEP * SCALE);
+const uint16_t alpha_q = (uint16_t) (ALPHA * SCALE);
+const uint16_t alpha_complement = 0b0111111111111111; // closest q15 value to 1
+
 
 
 #endif	/* CONSTANTS_H */
--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include <limits.h>
 #include "constants.h"
 #include "dsp.h"
+#include "vendor_dsp/include/dsp.h"
 
 #pragma config FNOSC = FRC // set Fast RC as oscillator, default is 7.27 MHz
 #pragma config FWDTEN = OFF // give SF control over disabling watch
@@ -29,6 +30,7 @@ int main(void) {
     uint32_t result = 0;
     uint16_t *result_p_upper = (uint16_t *)&result + 1;
     
+    
     printf("Q number 1: 0b");
     print_bin(q_num_1, sizeof(q_num_1)* 8);
     printf("Q number 2: 0b");
@@ -37,17 +39,31 @@ int main(void) {
     printf("Q1 x Q2 = 0b");
     CORCON &= ~(1); // explicitely enable fractional multiply for dsp (should be default)
     
+    // IIR low pass filter
+    // y[n] = alpha * x[n] + (1 - alpha) * y[n - 1]
+    // result is 0 initialized
+    // therefore we can jump straight into MAC loop
+    // for first order IIR we cannot benefit from MAC prefetching, since x[n] is a constant and we have to wait for y[n-1] to finish computing 
     asm(
         "clr A\n"
         "mov %1, w4\n"
         "mov %2, w5\n"
-        "mac w4*w5, A\n"
+        "mov %3, w6\n"
+        "mov %4, w7\n"
+        "mpy w4* w5, A\n" // alpha * x[n]
+        "sac A, w5\n"
+        "do #1, loop_end\n" // loop 2x
+        "mac w6* w7, A\n"
+        "sac A, w7\n" // store result for next itteration
+        "mov w5, ACCAH\n"
+
+        "loop_end:"
         "sac A, [%0]\n" // store upper word of A
         "SFTAC A, #-16\n" // shift accumulator up by 16
         "sac A, [--%0]\n" // store other half of A
         : "+r" (result_p_upper)
-        : "r" (q_num_1), "r" (q_num_2)
-        :"w4", "w5", "A"
+        : "r" (alpha_q), "r" (step_q), "r" (alpha_complement), "r" (result)
+        :"w4", "w5", "A", "w6", "w7"
     );
     print_bin(result, sizeof(result) * 8U);
     float result_f = Fract2Float((fractional) q_num_1);
